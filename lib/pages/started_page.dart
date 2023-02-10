3910f20e45a454c724407c3171ae3d5fe7c895be
s4llm4n <55424625+s4llm4n@users.noreply.github.com>
2023-02-10 11:09:30 +0700

    commit theme

--- lib/pages/started_page.dart
+++ lib/pages/started_page.dart
@@ -1,63 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:job_application/pages/signin_pages.dart';
 
 class GetStartedPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/bg.png'),
-              fit: BoxFit.cover,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/bg.png'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-            top: 60,
-            right: 30,
-            left: 30,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Build Your Next \nFuture Career Like \na Master',
-              style: GoogleFonts.poppins(
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.w400,
-                ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 60,
+                right: 30,
+                left: 30,
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Text('10,000 jobs Available',
-              style: GoogleFonts.poppins(
-                color: Colors.white,
-                fontSize: 14,
-                fontWeight: FontWeight.w300,
-              ),
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 500),
-                  child: Container(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Get Started',
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Build Your Next \nFuture Career Like \na Master',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    '10,000 jobs Available',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 500),
+                      child: Container(
+                        width: 200,
+                        height: 45,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            backgroundColor: Color(0xffFFFFFF),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(66),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Get Started',
+                            style: GoogleFonts.poppins(
+                              color: Color(0xff4141A4),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Container(
+                        width: 200,
+                        height: 45,
+                        child: OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignInPages()));
+                            },
+                            style: OutlinedButton.styleFrom(
+                              side: BorderSide(
+                                color: Colors.white,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(66),
+                              ),
+                            ),
+                            child: Text(
+                              'Sign In',
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            )),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ],),
+        ],
+      ),
     );
   }
 }
