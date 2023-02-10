4bb305a4c260081da56176c64ac344e464a21e9c
s4llm4n <55424625+s4llm4n@users.noreply.github.com>
2023-02-10 13:27:10 +0700

    commit img

--- lib/pages/signin_pages.dart
+++ lib/pages/signin_pages.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:job_application/theme.dart';
 
 class SignInPages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(
+            top: 60.0,
+            left: 24,
+            right: 24,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Sign In',
+                style: titleTextStyle,
+              ),
+              Text(
+                'Build Your Career',
+                style: subTitleTextStyle,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/illustration_one.png',
+                      width: 262,
+                      height: 240,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -9,3 +9,5 @@ TextStyle splashTextStyle = GoogleFonts.poppins(
 TextStyle titleTextStyle = GoogleFonts.poppins(
     color: Color(0xffB3B5C4), fontSize: 16, fontWeight: FontWeight.w400);
 
+TextStyle subTitleTextStyle = GoogleFonts.poppins(
+    color: Color(0xff272C2F), fontSize: 24, fontWeight: FontWeight.w600);
